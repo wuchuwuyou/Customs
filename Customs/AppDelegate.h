1bcf7b9f3299d12fccda1dc017fb6daada9d666e
WangTeng <woaijia_xxx@126.com>
2015-06-09 18:01:46 +0800

    init

---
+++ Customs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Customs
+//
+//  Created by Tiny on 15/6/9.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Customs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Customs
+//
+//  Created by Tiny on 15/6/9.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
