8cf47a1a12fcc8dbe2b9aa6816600a82b96804e2
WangTeng <woaijia_xxx@126.com>
2015-06-11 11:38:33 +0800

    init

---
+++ Customs/ViewModel/MWViewModel.h
@@ -0,0 +1,13 @@
+//
+//  MWViewModel.h
+//  Customs
+//
+//  Created by Tiny on 15/6/11.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#ifndef Customs_MWViewModel_h
+#define Customs_MWViewModel_h
+
+
+#endif
