3be9d4bcc0a9234e4d349be12b4dbd8bbcd8b00b
WangTeng <woaijia_xxx@126.com>
2015-06-23 22:10:07 +0800

    添加进出口税则查询列表页

---
+++ Customs/VC/MWIAETTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MWIAETTableViewController.h
+//  Customs
+//
+//  Created by Tiny on 15/6/23.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import "MWBaseTableViewController.h"
+
+@interface MWIAETTableViewController : MWBaseTableViewController
+
+@end
