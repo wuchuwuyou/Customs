70c4e3dcca13a6eb6484217ed8d567543a4f8f89
WangTeng <woaijia_xxx@126.com>
2015-06-23 22:08:39 +0800

    添加tableview 基类

---
+++ Customs/Base/MWBaseTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MWBaseTableViewController.h
+//  Customs
+//
+//  Created by Tiny on 15/6/23.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MWBaseTableViewController : UITableViewController
+
+@end
