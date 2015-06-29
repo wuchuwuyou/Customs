854176806d0d53462c3da7c0793386bad055dacd
WangTeng <woaijia_xxx@126.com>
2015-06-29 23:34:45 +0800

    添加公共协议类

---
+++ Customs/Helper/MWProtocols.h
@@ -0,0 +1,13 @@
+//
+//  MWProtocols.h
+//  Customs
+//
+//  Created by Tiny on 15/6/29.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MWProtocols <NSObject>
+
+@end
