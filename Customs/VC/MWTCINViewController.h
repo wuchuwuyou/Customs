2d6595f4fcc107cf2c4f82b7d39f9328f23d72f5
WangTeng <woaijia_xxx@126.com>
2015-07-02 15:51:53 +0800

    添加税则商品及品目注释查询页面

---
+++ Customs/VC/MWTCINViewController.h
@@ -0,0 +1,13 @@
+//
+//  MWTCINViewController.h
+//  Customs
+//
+//  Created by Tiny on 15/7/2.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import "MWBaseViewController.h"
+
+@interface MWTCINViewController : MWBaseViewController
+
+@end
---
+++ Customs/VC/MWTCINViewController.m
@@ -0,0 +1,37 @@
+//
+//  MWTCINViewController.m
+//  Customs
+//
+//  Created by Tiny on 15/7/2.
+//  Copyright (c) 2015年 Murphy. All rights reserved.
+//
+
+#import "MWTCINViewController.h"
+
+@interface MWTCINViewController ()
+
+@end
+
+@implementation MWTCINViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
